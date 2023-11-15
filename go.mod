37c5eccbd6c77f6e00dc61c6d8c5b8e7146d5b57
Evantf1 <efrisbie2232@gmail.com>
2023-11-14 20:42:04 -0500

    Alpha V0.0.1 GO Management Server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sp-manager-alpha-0-1
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+)
+
+const keyServerAddr = "serverAddr"
+
+func operationsCheck(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got / request... server operational\n", ctx.Value(keyServerAddr))
+	io.WriteString(w, "Server Operational\n")
+}
+
+func getStats(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query()
+
+	statType := url.Get("type")
+
+	switch statType {
+	case "home":
+		io.WriteString(w, "Home stats... Delivered.\n")
+		break
+	case "nfl":
+		io.WriteString(w, "NFL stats... Delivered.\n")
+		break
+	default:
+		io.WriteString(w, "No Type Provided... Defaulting to Home... Delivered.\n")
+	}
+
+}
+
+func getInfo(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query()
+	user := url.Get("id")
+
+	response := fmt.Sprintf("The user id is: %s\n", user)
+	io.WriteString(w, response)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", operationsCheck)
+	mux.HandleFunc("/stats", getStats)
+	mux.HandleFunc("/user", getInfo)
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+
+	serverOne := &http.Server{
+		Addr:    ":5000",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
+}
